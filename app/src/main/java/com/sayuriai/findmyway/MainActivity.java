5099d11b43ee6f645d035a86e15ebfb107295186
dancanmatara <dancangwe@gmail.com>
2020-08-11 23:34:30 +0200

    First commit

---
+++ app/src/main/java/com/sayuriai/findmyway/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sayuriai.findmyway;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+       if(user == null){
+           //Go to login page
+           Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+           startActivity(intent);
+           finish();
+       }
+       else{
+
+       }
+
+
+    }
+
+}
